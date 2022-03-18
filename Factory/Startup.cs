0094a717678d36d2138f142e6846f0278dc2d360
S. Espinet <s.lou.espinet@gmail.com>
2022-03-18 08:31:09 -0700

    create/name project base files

--- Factory/Startup.cs
+++ Factory/Startup.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using TemplateDatabaseBasics.Models;
+using Factory.Models;
 
-namespace TemplateDatabaseBasics
+namespace Factory
 {
   public class Startup
   {
@@ -25,7 +25,7 @@ namespace TemplateDatabaseBasics
       services.AddMvc();
 
       services.AddEntityFrameworkMySql()
-        .AddDbContext<TemplateDatabaseBasicsContext>(options => options
+        .AddDbContext<FactoryContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     }
 
