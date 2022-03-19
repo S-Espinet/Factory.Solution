4df685167ff841012e3ee809f34512b3a98a29ca
S. Espinet <s.lou.espinet@gmail.com>
2022-03-18 19:01:48 -0700

    clean up styling and typos

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 
 namespace Factory.Models
@@ -11,6 +12,7 @@ namespace Factory.Models
     }
 
     public int EngineerId { get; set; }
+    [DisplayName("Name")]
     public string EngineerName { get; set; }
     public DateTime HireDate { get; set; }
 
